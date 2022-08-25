1a6fadbf4bf2ce4c2a63145be0f981dbb80d8c9e
Nitish Kafle <kaflenit@gmail.com>
2022-08-25 17:27:00 -0500

    add data

---
+++ src/data/rewardData.js
@@ -0,0 +1,142 @@
+export const rewardData = [
+  {
+    transactionId: 20165,
+    transactionDate: 1578071968,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 62,
+        itemName: "Coffee",
+        price: 40,
+        quantity: 2,
+      },
+    ],
+  },
+  {
+    transactionId: 21142,
+    transactionDate: 1578590368,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 2039,
+        itemName: "Chocolate Bar",
+        price: 16.99,
+        quantity: 5,
+      },
+      {
+        itemId: 149,
+        itemName: "Keyboard",
+        price: 89.99,
+        quantity: 1,
+      },
+    ],
+  },
+  {
+    transactionId: 39165,
+    transactionDate: 1579713561,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 593,
+        itemName: "Mouse",
+        price: 48.99,
+        quantity: 1,
+      },
+      {
+        itemId: 4,
+        itemName: "Monitor",
+        price: 129.99,
+        quantity: 2,
+      },
+    ],
+  },
+  {
+    transactionId: 14923,
+    transactionDate: 1581106408,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 88930,
+        itemName: "Water Bottle",
+        price: 29.99,
+        quantity: 10,
+      },
+      {
+        itemId: 62,
+        itemName: "Coffee",
+        price: 15.99,
+        quantity: 3,
+      },
+      {
+        itemId: 209,
+        itemName: "Xfinity Internet",
+        price: 99.99,
+        quantity: 1,
+      },
+      {
+        itemId: 9384,
+        itemName: "Macbook Pro",
+        price: 1299,
+        quantity: 1,
+      },
+    ],
+  },
+  {
+    transactionId: 48995,
+    transactionDate: 1581873508,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 9301,
+        itemName: "Pizza",
+        price: 10.99,
+        quantity: 2,
+      },
+    ],
+  },
+  {
+    transactionId: 49999,
+    transactionDate: 1583086708,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 390,
+        itemName: "PC Repair",
+        price: 129.99,
+        quantity: 1,
+      },
+      {
+        itemId: 1213,
+        itemName: "Macbook Pro M2",
+        price: 1299,
+        quantity: 1,
+      },
+      {
+        itemId: 45634,
+        itemName: "Airpods Pro",
+        price: 549.99,
+        quantity: 1,
+      },
+    ],
+  },
+
+  {
+    transactionId: 51345,
+    transactionDate: 1583687908,
+    customerId: 1,
+    purchases: [
+      {
+        itemId: 5493,
+        itemName: "Costco Membership",
+        price: 69.99,
+        quantity: 1,
+      },
+      {
+        itemId: 31,
+        itemName: "Gas",
+        price: 79.99,
+        quantity: 1,
+      },
+    ],
+  },
+];
