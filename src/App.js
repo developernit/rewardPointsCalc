b7ff2fc173ffea657fc4d97cb963cc210cd4cd38
Nitish Kafle <kaflenit@gmail.com>
2022-08-25 17:26:38 -0500

    setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { RewardTable } from "./components/RewardTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Container maxW={"container.xl"}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/1" />} />
+            <Route path=":currentMonth" element={<RewardTable />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </ChakraProvider>
   );
 }
 
